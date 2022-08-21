5dd601ef7a15178b78cdb96736df9423a91d41f5
andreyshveduke <shveduke@mail.ru>
2022-08-21 20:32:23 +0300

    to find error in ipynb

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -20,26 +20,8 @@ def random_predict(number: int = 1) -> int:
     #print (f"комп угадал сам себя за {count} попыток")
     return(count)
 
-#random_predict(50)
 
-def score_game(effective_predict) -> int:
-    """Расчитывает среднее  за которое будет найдено  загаданное число от 1 до 100 
 
-    Args:
-        random_predict (_type_): Функция которая угадывает число
-
-    Returns:
-        int: среднее из резултьтатов 1000 попыток
-    """
-    count_ls = []
-    random_array = np.random.randint(1,101, size=(1000)) #создаем список из 1000 number
-    
-    for number in random_array:
-        count_ls.append(effective_predict(number)) # добавляем к списку попыток очередной резултьат работы функции effective_predict
-    
-    score = int(np.mean(count_ls))
-    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
-    return (score)
 
 def effective_predict(number: int=1) -> int:
     """Функция угадывает число меньше чем за 20 попыток
@@ -75,6 +57,29 @@ def effective_predict(number: int=1) -> int:
             break
     #print (f"комп угадал сам себя за {count} попыток")
     return(count)           
+
+
+
+def score_game(predict) -> int:
+    """Расчитывает среднее  за которое будет найдено  загаданное число от 1 до 100 
+
+    Args:
+        random_predict (_type_): Функция которая угадывает число
+
+    Returns:
+        int: среднее из резултьтатов 1000 попыток
+    """
+    count_ls = []
+    random_array = np.random.randint(1,101, size=(1000)) #создаем список из 1000 number
+    
+    for number in random_array:
+        count_ls.append(predict(number)) # добавляем к списку попыток очередной резултьат работы функции 
+    
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+    return (score)
+
+
 
 if __name__ == "__main__":
     score_game( effective_predict )
