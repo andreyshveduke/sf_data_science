033b67f5dd8efb6d0203b91d4d2e6dbdd92acd2f
andreyshveduke <shveduke@mail.ru>
2022-08-21 15:33:48 +0300

    more pep8 in README.md game_v2.py

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def random_predict(number: int=1) -> int:
-    """РАндомно угадываем число
+    """Рандомно угадываем число
 
     Args:
         number (int, optional): Загаданное число . Defaults to 1.
@@ -23,15 +23,22 @@ def random_predict(number: int=1) -> int:
 #random_predict(50)
 
 def score_game(random_predict) -> int:
-    
+    """Расчитывает среднее  за которое будет найдено  загаданное число от 1 до 100 
+
+    Args:
+        random_predict (_type_): Функция которая угадывает число
+
+    Returns:
+        int: среднее из резултьтатов 1000 попыток
+    """
     count_ls=[]
-    random_array= np.random.randint(1,100, size= (1000))
+    random_array= np.random.randint(1,101, size= (1000)) #создаем список из 1000 number
     
     for number in random_array:
-        count_ls.append(random_predict(number))
+        count_ls.append(random_predict(number)) # добавляем к списку попыток очередной резултьат работы функции random_predict
     
     score = int(np.mean(count_ls))
-    print(f"число попыток = {score}")
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
     return (score)
 
 if __name__ =="__main__":
